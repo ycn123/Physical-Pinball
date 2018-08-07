82d090eb69e9d057d4d106094f9f8d182b1c14b7
ycn123 <32393132+ycn123@users.noreply.github.com>
2018-08-07 22:47:21 +0800

    场景建立

---
+++ Physical Pinball Demo/Assets/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Rigidbody2D _rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var _direction = mousePos - transform.position;
+            _rigidbody2D.AddForce();
+        }
+    }
+}
